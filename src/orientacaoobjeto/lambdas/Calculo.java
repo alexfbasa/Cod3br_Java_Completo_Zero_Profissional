a559ef1eebb1b0fa4acf483b82cbc6340383cb27
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-01-25 10:33:24 +0200

    Criando Stream

--- src/orientacaoobjeto/lambdas/Calculo.java
+++ src/orientacaoobjeto/lambdas/Calculo.java
@@ -1,6 +1,14 @@
 package orientacaoobjeto.lambdas;
-
+@FunctionalInterface
 public interface Calculo {
-
-    double executar(double a, double b);
+	// Uma funcao de Interface soh pode ter apenas um metodo abstrato
+	double executar(double a, double b); // Esse eh um metodo abstrato
+	// double imprimir(double numero); Ao criar isso, tudo para de funcionar
+	// Contudo, uma interface pode ter metodo defaul e static
+	default String legal() {
+		return "opa";
+	}
+	static String muitoLegal() {
+		return "muito legal";
+	}
 }
