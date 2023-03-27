1a32597b7e9fe4d365b71ee1ec0aeea5c5d3589f
AlexBasa <alexsimple@gmail.com>
2023-03-27 22:33:03 +0300

    Commit cinco

--- src/classesmetodos/AreaCirc.java
+++ src/classesmetodos/AreaCirc.java
@@ -4,6 +4,7 @@ public class AreaCirc {
 
 	double raio;
 	// Pi sendo atributo de instância
+	// static faz o atribo ser de instância geralmente ele é final
 	static double PI = 3.14;
 
 	AreaCirc(double raioInicial) {
